0cbe4dbc4a5cf49f06707434921dda5ec03d613a
munimrahman <munimrh@gmail.com>
2022-07-23 19:41:52 +0600

    login page done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import "../styles/App.css";
 import Layout from "./Layout";
+import Login from "./pages/Login";
 // import Home from "./pages/Home";
-import Signup from "./pages/Signup";
+// import Signup from "./pages/Signup";
 function App() {
   return (
     <Layout>
       {/* <Home /> */}
-      <Signup />
+      {/* <Signup /> */}
+      <Login />
     </Layout>
   );
 }
---
+++ src/components/pages/Login.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classes from "../../styles/Login.module.css";
+import Button from "../Button";
+import Form from "../Form";
+import Illustration from "../Illustration";
+import TextInput from "../TextInput";
+
+const Login = () => {
+  return (
+    <>
+      <h1>Login to your account</h1>
+      <div class="column">
+        <Illustration />
+        <Form className={`${classes.login}`}>
+          <TextInput
+            type="text"
+            placeholder="Enter Name"
+            icon="alternate_email"
+          />
+          <TextInput type="password" placeholder="Enter Password" icon="lock" />
+          <Button>Submit Now</Button>
+          <div class="info">
+            Don't have an account? <a href="signup.html">Signup</a> instead.
+          </div>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
